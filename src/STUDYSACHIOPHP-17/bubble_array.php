99594c3f446f7d55980552b26e0dd3fd11e49455
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-01-30 22:14:38 +0900

    STUDYSACHIOPHP-17 ランダムな値を生成

--- src/STUDYSACHIOPHP-17/bubble_array.php
+++ src/STUDYSACHIOPHP-17/bubble_array.php
@@ -4,11 +4,14 @@ function array_return ($input){
 //ループ処理で$inputの数だけ繰り返し処理を行なう
     for($i = 1; $i <= $input; $i++){
         $result[] = $i;
+        $rand_result = array_rand($result,1);
     }
-    return $result;
+    var_dump($rand_result);
+    return $rand_result;
 }
 
 //$result_funcに関数の実行結果を代入
 $result_func = array_return(10);
+// var_dump($result_func);
 //[]内はランダムな値による
-echo $result_func[];
+// echo $result_func;
