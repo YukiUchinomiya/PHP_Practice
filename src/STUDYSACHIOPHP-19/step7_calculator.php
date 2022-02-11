bef921b787f5731ae7306ec5e5c8f0c5e46152cc
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-02-11 16:35:18 +0900

    STUDYSACHIOPHP-17,18 指摘事項部分の修正

--- src/STUDYSACHIOPHP-19/step7_calculator.php
+++ src/STUDYSACHIOPHP-19/step7_calculator.php
@@ -2,18 +2,20 @@
 
 // STEP7: STEP6を変更し、Switch文を利用して、特定の文字（'=', '.', 'c') の場合はその文字を出力してみる
 
-echo 'STEP7 Please enter: ';
-$array_std7 = trim(fgets(STDIN));
-switch($array_std7){
-    case '=':
-        echo '=';
-        break;
-    case '.':
-        echo '.';
-        break;
-    case 'c':
-        echo 'c';
-        break;
-    default:
-    return;
+while(true){
+    echo 'STEP7 Please enter: ';
+    $array_std7 = trim(fgets(STDIN));
+    switch($array_std7){
+        case '=':
+            echo '=' . PHP_EOL;
+            break;
+        case '.':
+            echo '.' . PHP_EOL;
+            break;
+        case 'c':
+            echo 'c' . PHP_EOL;
+            break;
+        default:
+        return;
+    }
 }
--- src/STUDYSACHIOPHP-19/step8_calculator.php
+++ src/STUDYSACHIOPHP-19/step8_calculator.php
@@ -2,36 +2,32 @@
 
 // STEP8: STEP7に加え、演算子を受け取った場合の分岐を追加してみる
 //想定している結果が出力されている(想定したcase毎で出力される)
-// echo 'STEP8 Please enter: ';
-// $array_std8 = trim(fgets(STDIN));
-// switch($array_std8){
-//     case '=':
-//         echo '=';
-//         break;
-//     case '.':
-//         echo '.';
-//         break;
-//     case 'c':
-//         echo 'c';
-//         break;
-//     case '+':
-//         echo '+';
-//         break;
-//     case '-':
-//         echo '-';
-//         break;
-//     case '*':
-//         echo '*';
-//         break;
-//     case '/':
-//         echo '/';
-//         break;
-//     case '%':
-//         echo '%';
-//         break;
-//     case '**':
-//         echo '**';
-//         break;
-//     default:
-//     return;
-// }
+while(true){
+    echo 'STEP8 Please enter: ';
+    $array_std8 = trim(fgets(STDIN));
+    switch($array_std8){
+        case '=':
+            echo '=' . PHP_EOL;
+            break;
+        case '.':
+            echo '.' . PHP_EOL;
+            break;
+        case 'c':
+            echo 'c' . PHP_EOL;
+            break;
+        case '+':
+            echo '+' . PHP_EOL;
+            break;
+        case '-':
+            echo '-' . PHP_EOL;
+            break;
+        case '*':
+            echo '*' . PHP_EOL;
+            break;
+        case '/':
+            echo '/' . PHP_EOL;
+            break;
+        default:
+        return;
+    }
+}
