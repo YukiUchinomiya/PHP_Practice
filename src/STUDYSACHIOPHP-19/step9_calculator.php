a8c414d946b83c55f01b69e69caf27b9307e44da
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-02-11 22:19:20 +0900

    STUDYSACHIOPHP-19 [WIP]逆ポーランド電卓の作成

--- src/STUDYSACHIOPHP-19/step9_calculator.php
+++ src/STUDYSACHIOPHP-19/step9_calculator.php
@@ -1,40 +1,48 @@
 <?php
 
 
-//TODO::作業途中
 // STEP9: STEP8に加え、受け取った演算子で逆ポーランド計算を行ってみる
 //逆ポーランド記法とは先頭から順に値を読んで、算術演算子が現れるまで数字を１つずつ配列に格納すること
 //数字を繰り返し配列に格納していき、入力値が演算子のケースの場合、それまでに格納されていた配列の値を
 //入力された演算子で計算するような仕様の認識
 
-echo 'STEP9 Please enter: ';
-$stack = [];
-$stack = $array_std8;
-$array_std8 = trim(fgets(STDIN));
-switch($array_std8){
-    case '=':
-        echo '=';
-        break;
-    case '.':
-        echo '.';
-        break;
-    case 'c':
-        echo 'c';
-        break;
-    case '+':
-        $stack;
-    case '-':
-        $stack;
-    case '*':
-        $stack;
-    case '/':
-        $stack;
-    case '%':
-        $stack;
-    case '**':
-        $stack;
-    default:
-    return;
+while(true){
+    echo 'STEP9 Please enter: ';
+    $array_std9 = trim(fgets(STDIN));
 
+//配列に入力された数値を格納
+    if(is_numeric($array_std9)){
+        $array_result[] = $array_std9;
+        var_dump($array_result);
+    }
+
+//演算子が入力された場合、数値が格納された配列の数値で計算する
+    switch($array_std9){
+        case '=':
+            echo '=' . PHP_EOL;
+            break;
+        case '.':
+            echo '.' . PHP_EOL;
+            break;
+        case 'c':
+            echo 'c' . PHP_EOL;
+            break;
+        case '+':
+        //ここで格納された配列の数値に対して計算を行う
+            $a = array_slice($array_result,0);
+            $b = array_slice($array_result,1);
+            $c = $a + $b;
+            var_dump($c);
+            break;
+        case '-':
+        //ここで格納された配列の数値に対して計算を行う
+            break;
+        case '*':
+        //ここで格納された配列の数値に対して計算を行う
+            break;
+        case '/':
+        //ここで格納された配列の数値に対して計算を行う
+            break;
+    }
 }
 
