3140729c1813f399741744259723bf7a047d836f
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-02-14 21:49:39 +0900

    STUDYSACHIOPHP-19　逆ポーランド電卓の作成

--- src/STUDYSACHIOPHP-19/calculator.php
+++ src/STUDYSACHIOPHP-19/calculator.php
@@ -4,6 +4,7 @@
 // コマンドラインツールとして、利用できる形にする
 
 //STEP1~12のまとめ
+//各ケースでテスト済み
 
 while(true){
     echo '数値もしくは演算子を入力してください。 ' . PHP_EOL;
@@ -11,13 +12,13 @@ while(true){
     echo ">";
     $array_std12 = trim(fgets(STDIN)) ;
 
-    //配列に入力された数値を格納
+    //入力された値が数値であればを格納する
     if(is_numeric($array_std12)){
-        //スタックを準備
+        //スタックを準備してpush
         $array_result[] = $array_std12;
         print_r($array_result) . PHP_EOL;
     }else{
-        //演算子が入力された場合、数値が格納された配列の数値で計算する
+        //数値以外が入力された場合、数値が格納された配列の数値で計算する
         switch($array_std12){
             case '=':
                 echo "計算結果:" . $array_result  . PHP_EOL;
@@ -29,35 +30,37 @@ while(true){
                 $array_result = [];
                 break;
             case '+':
-            //配列の最初、最後の要素を取り出して足す
-            $last_array = array_pop($array_result);
-            $first_array = array_pop($array_result);
-            $array_result = $first_array + $last_array;
-            echo $array_result . PHP_EOL;
+            //配列の末尾から要素を２つ取り出してその2つで計算後、計算した値を配列に返す
+            $pop1_array = array_pop($array_result);
+            $pop2_array = array_pop($array_result);
+            $array_result[] = $pop2_array + $pop1_array;
+            print_r($array_result) . PHP_EOL;
                 break;
             case '-':
-            //配列の最初、最後の要素を取り出して引く
-            $last_array = array_pop($array_result);
-            $first_array = array_pop($array_result);
-            $array_result = $first_array - $last_array;
-            echo $array_result . PHP_EOL;
+            //配列の末尾から要素を２つ取り出してその2つで計算後、計算した値を配列に返す
+            $pop1_array = array_pop($array_result);
+            $pop2_array = array_pop($array_result);
+            $array_result[] = $pop2_array - $pop1_array;
+            print_r($array_result) . PHP_EOL;
                 break;
             case '*':
-            //配列の最初、最後の要素を取り出してかける
-            $last_array = array_pop($array_result);
-            $first_array = array_pop($array_result);
-            $array_result = $first_array * $last_array;
-            echo $array_result . PHP_EOL;
+            //配列の末尾から要素を２つ取り出してその2つで計算後、計算した値を配列に返す
+            $pop1_array = array_pop($array_result);
+            $pop2_array = array_pop($array_result);
+            $array_result[] = $pop2_array * $pop1_array;
+            print_r($array_result) . PHP_EOL;
                 break;
             case '/':
-            //配列の最初、最後の要素を取り出して割る
-            $last_array = array_pop($array_result);
-            $first_array = array_pop($array_result);
-            if($last_array === 0){
+            //配列の末尾から要素を２つ取り出してその2つで計算後、計算した値を配列に返す
+            $pop1_array = array_pop($array_result);
+            $pop2_array = array_pop($array_result);
+            //値０で割り算を行う場合配列を空にする
+            if($pop1_array == 0 || $pop2_array == 0){
                $array_result = [];
+               echo "配列をクリアします。" . PHP_EOL;
             }else{
-                $array_result = $first_array / $last_array;
-                echo $array_result . PHP_EOL;
+                $array_result[] = $pop2_array / $pop1_array;
+                print_r($array_result) . PHP_EOL;
                 break;
             }
         }
