30be4664db3143f99c025705992cf597ac395316
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-02-16 23:09:04 +0900

    STUDYSACHIOPHP-19 [WIP] 逆ポーランド電卓の作成

--- src/STUDYSACHIOPHP-19/calculator.php
+++ src/STUDYSACHIOPHP-19/calculator.php
@@ -1,5 +1,4 @@
 <?php
-//TODO::作業途中（PR指摘事項部分の修正途中
 // 逆ポーランド電卓の作成
 // コマンドラインツールとして、利用できる形にする
 
@@ -20,9 +19,14 @@ while(true){
         //数値以外が入力された場合、数値が格納された配列の数値で計算する
         switch($standard_io){
             case '=':
-                echo "計算結果：";
-                print_r($array_result) . PHP_EOL;
-                break;
+                if(count($array_result) == 1){
+                    echo "計算結果：";
+                    print_r($array_result) . PHP_EOL;
+                    break;
+                }else{
+                    echo "計算途中です。" . PHP_EOL;
+                    break;
+                }
             case '.':
                 return;
                 break;
@@ -52,12 +56,11 @@ while(true){
                 $a = array_pop($array_result);
                 $b = array_pop($array_result);
                 //値０で割り算を行う場合配列を空にする
-                if($a == 0 || $b == 0){
+                if($a == 0){
                     $array_result = [];
                     echo "配列をクリアします。" . PHP_EOL;
                 }else{
                     $array_result[] = $b / $a;
-
                 break;
                 }
         }
