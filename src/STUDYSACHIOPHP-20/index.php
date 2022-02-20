c6dbcff8ff760b094c02ec0d212b0b2106e04ae4
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-02-21 00:08:26 +0900

    STUDYSACHIOPHP-20 家計簿アプリの作成

---
+++ src/STUDYSACHIOPHP-20/index.php
@@ -0,0 +1,22 @@
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>家計簿アプリ</title>
+</head>
+<body>
+<!--ヘッダー-->
+<header>
+<h1>aaaa</h1>
+</header>
+<!--項目、金額入力-->
+<section>
+</section>
+<!--登録した最新10件を表示-->
+<section>
+</section>
+<script src="js/main.js"></script>
+</body>
+</html>
